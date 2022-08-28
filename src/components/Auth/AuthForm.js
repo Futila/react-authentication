9e2642a85cbd09b6f3cb71e37f973ee85000bd7b
Fernando Futila <fernando.futila@gmail.com>
2022-08-27 23:19:57 -0300

    Adding user Login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -20,34 +20,43 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC1-mfMWw_EbgVbA5fKF8dtacDU7UaiJbA";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC1-mfMWw_EbgVbA5fKF8dtacDU7UaiJbA",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC1-mfMWw_EbgVbA5fKF8dtacDU7UaiJbA";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed";
 
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => console.log(data))
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
